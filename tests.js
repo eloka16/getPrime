3521545b3efa6d394daedbe0fb453927cf1d5f90
frank chukwu <frank chukwu>
2017-01-09 11:16:58 +0100

    write getprime function to pass the test cases

--- tests.js
+++ tests.js
@@ -6,23 +6,23 @@ var assert = chai.assert;
 var lib = require('./lib/getPrime.js');
 
 describe("Test that returns prime numbers in an array from 0 to the number", function() {
-  it("should return [2, 3, 5, 7] for 10", function() {
-    assert(lib.getPrime(10) == [2, 3, 5, 7]);
+  it("should return [2, 3, 5, 7] for 10", function() {  
+    assert.deepEqual(lib.getPrime(10), [2, 3, 5, 7]);
   });
   it("should return [2] for 2", function() {
-    assert(lib.getPrime(2) == [2]);
+    assert.deepEqual(lib.getPrime(2),  [2]);
   });
   it("should return [2, 3, 5] for 5", function() {
-    assert(lib.getPrime(5) == [2, 3, 5]);
+    assert.deepEqual(lib.getPrime(5), [2, 3, 5]);
   });
-  it("should return [ 2, 3, 5, 7, 11, 13, 17 ] for 17", function() {
-    assert(lib.getPrime(17) == [ 2, 3, 5, 7, 11, 13, 17 ]);
+  it("should return [2, 3, 5, 7, 11, 13, 17] for 17", function() {
+    assert.deepEqual(lib.getPrime(17), [2, 3, 5, 7, 11, 13, 17]);
   });
-  it("should return [ 2, 3, 5, 7, 11, 13] for 13", function() {
-    assert(lib.getPrime(13) == [ 2, 3, 5, 7, 11, 13]);
+  it("should return  2, 3, 5, 7, 11, 13] for 13", function() {
+    assert.deepEqual(lib.getPrime(13), [2, 3, 5, 7, 11, 13]);
   });
-  it("should return [ 2, 3, 5, 7, 11, 13, 17, 19 ] for 20", function() {
-    assert(lib.getPrime(20) == [ 2, 3, 5, 7, 11, 13, 17, 19]);
+  it("should return  2, 3, 5, 7, 11, 13, 17, 19] for 20", function() {
+    assert.deepEqual(lib.getPrime(20), [2, 3, 5, 7, 11, 13, 17, 19]);
   });
 });
 
